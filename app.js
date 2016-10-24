99d991f37827c2e9cf44401d6559a5ec280679ff
mani <manivannan13tnj@gmail.com>
2016-10-24 23:53:15 +0530

    adding static files and htmls

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
-var Routes = require('./api-controller');
-var express = require("express");
-var path = require('path');
+var Routes   = require('./api-controller');
+var Htmls    = require('./htmls/controller');
+var express  = require("express");
+var path     = require('path');
 
 //var csrf = require('csurf');  
 //var cookieParser = require('cookie-parser')
@@ -13,9 +14,7 @@ mongoose.connect(connStr, function(err) {
     console.log('Successfully connected to MongoDB');
 });
 
-
 //var csrfProtection = csrf({ cookie: true });  
-
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,20 +23,19 @@ app.use(bodyParser.text())
 // we need this because "cookie" is true in csrfProtection 
 //app.use(cookieParser());
 
-
-var apiRoutes = express.Router(); 
 app.set('superSecret', "!8765fahjds32"); // secret variable
 
-Routes.init(apiRoutes,app);
-//Routes.init(apiRoutes);
+var apiRouter = express.Router(); 
+Routes.init(apiRouter,app);
+app.use('/api', apiRouter); // apply the routes to our application with the prefix /api
 
-// apply the routes to our application with the prefix /api
-app.use('/api', apiRoutes);
+var htmlRouter = express.Router(); 
+Htmls.init(htmlRouter,app);
+app.use(htmlRouter);
 
+// Public static content
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use("/public", express.static(path.join(__dirname, 'public')));
-
-
-app.listen(3000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000!');
 });
